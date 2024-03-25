b35310e6c1616e2f04a9b289ae879c59dc0ad245
Nikita Kleymenov <kleymenov.nikita@apriorit.com>
2024-03-25 13:12:41 +0200

    minor comment removed

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-# add sqlachemy model here
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
